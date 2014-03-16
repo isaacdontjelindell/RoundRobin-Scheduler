46ff4ebabb7e5d3b6dbd4375a82b84c15008b165
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-03-16 14:35:22 -0500

    processes now keep track of states and remaining times

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ import "fmt"
 
 
 func main() {
-    getInput()
+    //testProcess()
+
+    procList := getInitialProcList()
+
+    fmt.Println(procList)
 }
 
-func getInput() []Process {
-    fmt.Println("hello world")
-    p := Process{"P1"}
-    s := []Process{p}
-    return s
+
+func getInitialProcList() []Process {
+    p1 := Process{"P1", READY, []int{7, 2, 9, 6, 10}, 7}
+    p2 := Process{"P2", READY, []int{9, 4, 5, 3, 2}, 9}
+    p3 := Process{"P3", READY, []int{12, 5, 16, 7, 4}, 12}
+
+    ret := []Process{p1, p2, p3}
+
+    return ret
 }
