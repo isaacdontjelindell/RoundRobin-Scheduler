8fc9936c9db86725dfc89670c7bff9b55ddf4e53
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-03-18 10:29:03 -0500

    set up tick function

--- main.go
+++ main.go
@@ -1,23 +1,66 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+)
 
 
 func main() {
-    //testProcess()
+    readyQueue := make(chan Proc, 20)
+    runQueue := make(chan Proc, 20)
+    waitQueue := make(chan Proc, 20)
 
     procList := getInitialProcList()
-
     fmt.Println(procList)
+
+    for _, proc := range(procList) {
+        switch {
+            case proc.Status == READY:
+                readyQueue <- proc
+            case proc.Status == RUN:
+                runQueue <- proc
+            case proc.Status == WAIT:
+                waitQueue <- proc
+        }
+    }
+
+    for {
+        time.Sleep(time.Second)
+        tick(readyQueue, runQueue, waitQueue)
+    }
+}
+
+func tick(readyQueue chan Proc, runQueue chan Proc, waitQueue chan Proc) {
+    select {
+    case p := <-readyQueue:
+        fmt.Println(p)
+    default:
+        fmt.Println("[tick] nothing in ready queue")
+    }
+
+    select {
+    case p := <-waitQueue:
+        fmt.Println(p)
+    default:
+        fmt.Println("[tick] nothing in wait queue")
+    }
+
+    select {
+    case p := <-runQueue:
+        fmt.Println(p)
+    default:
+        fmt.Println("[tick] nothing in run queue")
+    }
 }
 
 
-func getInitialProcList() []Process {
-    p1 := Process{"P1", READY, []int{7, 2, 9, 6, 10}, 7}
-    p2 := Process{"P2", READY, []int{9, 4, 5, 3, 2}, 9}
-    p3 := Process{"P3", READY, []int{12, 5, 16, 7, 4}, 12}
+func getInitialProcList() []Proc {
+    p1 := Proc{"P1", READY, []int{7, 2, 9, 6, 10}, 7}
+    p2 := Proc{"P2", READY, []int{9, 4, 5, 3, 2}, 9}
+    p3 := Proc{"P3", READY, []int{12, 5, 16, 7, 4}, 12}
 
-    ret := []Process{p1, p2, p3}
+    ret := []Proc{p1, p2, p3}
 
     return ret
 }
--- proc.go
+++ proc.go
@@ -3,59 +3,59 @@ package main
 import "fmt"
 
 const (
-    READY = 0
-    RUNNING = 1
-    WAITING = 2
+	READY = 0
+	RUN   = 1
+	WAIT  = 2
 )
 
-type Process struct {
-    Name               string
-    Status             int
-    Times              []int
-    StateTimeRemaining int
+type Proc struct {
+	Name               string
+	Status             int
+	Times              []int
+	StateTimeRemaining int
 }
 
-func changeProcState(proc *Process) int {
-    if len(proc.Times) == 0 {
-        return 1
-    }
+func changeProcState(proc *Proc) int {
+	if len(proc.Times) == 0 {
+		return 1
+	}
 
-    newStateTime := proc.Times[0]
-    proc.Times = proc.Times[1:] // remove from head
-    proc.StateTimeRemaining = newStateTime
-    return 1
+	newStateTime := proc.Times[0]
+	proc.Times = proc.Times[1:] // remove from head
+	proc.StateTimeRemaining = newStateTime
+	return 1
 }
 
-func testProcess() {
-    var p Process
-    p.Name = "testp"
-    p.Status = READY
-    p.Times = []int{1, 2, 3, 4}
-    p.StateTimeRemaining = 1
+func testProc() {
+	var p Proc
+	p.Name = "testp"
+	p.Status = READY
+	p.Times = []int{1, 2, 3, 4}
+	p.StateTimeRemaining = 1
 
-    fmt.Println(p.Name)
-    fmt.Println(p.Status)
+	fmt.Println(p.Name)
+	fmt.Println(p.Status)
 
-    changeProcState(&p)
-    fmt.Println(p.Times)
-    fmt.Println(p.StateTimeRemaining)
+	changeProcState(&p)
+	fmt.Println(p.Times)
+	fmt.Println(p.StateTimeRemaining)
 
-    changeProcState(&p)
-    fmt.Println(p.Times)
-    fmt.Println(p.StateTimeRemaining)
+	changeProcState(&p)
+	fmt.Println(p.Times)
+	fmt.Println(p.StateTimeRemaining)
 
-    changeProcState(&p)
-    fmt.Println(p.Times)
-    fmt.Println(p.StateTimeRemaining)
+	changeProcState(&p)
+	fmt.Println(p.Times)
+	fmt.Println(p.StateTimeRemaining)
 
-    changeProcState(&p)
-    fmt.Println(p.Times)
-    fmt.Println(p.StateTimeRemaining)
+	changeProcState(&p)
+	fmt.Println(p.Times)
+	fmt.Println(p.StateTimeRemaining)
 
-    v := changeProcState(&p)
-    if v > 0 {
-        fmt.Println("ERROR: no more state times")
-    }
-    fmt.Println(p.Times)
-    fmt.Println(p.StateTimeRemaining)
+	v := changeProcState(&p)
+	if v > 0 {
+		fmt.Println("ERROR: no more state times")
+	}
+	fmt.Println(p.Times)
+	fmt.Println(p.StateTimeRemaining)
 }
