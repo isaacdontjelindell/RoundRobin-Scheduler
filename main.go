c03cfa10936455c80953e0eaaf01a6a3ff712766
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-03-20 13:59:23 -0500

    keep track of process non-I/O wait time

--- main.go
+++ main.go
@@ -1,132 +1,144 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
-const CLOCK_SPEED = time.Second/20
+const CLOCK_SPEED = time.Second / 30
 const QUANTUM = 10
 
 func main() {
 
-    procList := getInitialProcList()
-    fmt.Println(procList)
+	procList := getInitialProcList()
+	fmt.Println(procList)
 
-    runningList := make([]Proc, 0)
-    waitingList := make([]Proc, 0)
-    readyList := make([]Proc, 0)
+	runningList := make([]Proc, 0)
+	waitingList := make([]Proc, 0)
+	readyList := make([]Proc, 0)
 
-    // prime a proc into the running state
-    procList[0].newProcState() // yikes - unchecked operation. What if no procs??
-    runningList = append(runningList, procList[0])
-    for _, proc := range procList[1:] {
-        readyList = append(readyList, proc)
-    }
+	// prime a proc into the running state
+	procList[0].newProcState() // yikes - unchecked operation. What if no procs??
+	runningList = append(runningList, procList[0])
+	for _, proc := range procList[1:] {
+		readyList = append(readyList, proc)
+	}
 
-    run(readyList, waitingList, runningList)
+	run(readyList, waitingList, runningList)
 }
 
 func run(readyList []Proc, waitingList []Proc, runningList []Proc) {
-    systemTime := 1
-    quant := 0
+	systemTime := 1
+	quant := 0
 
-    done := false
+	doneList := make([]Proc, 0)
+	done := false
 
-    for !done {
-        //fmt.Printf("%d:\n", systemTime ) // for debugging
-        printRunning(runningList)
-        printReady(readyList)
-        printWaiting(waitingList)
+	for !done {
+		//fmt.Printf("%d:\n", systemTime ) // for debugging
+		printRunning(runningList)
+		printReady(readyList)
+		printWaiting(waitingList)
 
-        systemTime++
+		systemTime++
 
-        removeInd := make([]int, 0)
-        if !(len(waitingList) == 0) {
-            for i, _ := range waitingList {
-                waitingList[i].RemainingStateTime--
-                if waitingList[i].RemainingStateTime < 1 {
-                    removeInd = append(removeInd, i) // save index of proc that needs IO
-                }
-            }
-            // There's got to be a better way of doing this
-            // These are procs that are done w/ IO, ready to run
-            for i:=0; i < len(removeInd); i++ {
-                p := waitingList[i]
-                // cut out the i-th element from waitingList
-                waitingList = append(waitingList[:i], waitingList[i+1:]...)
-                readyList = append(readyList, p)
-            }
-       }
+        // keep track of non-I/O wait time for each proc
+		for i, _ := range readyList {
+	        //fmt.Printf("DEBUG: %s is waiting/ready\n", readyList[i])
+		    readyList[i].WaitTime++
+		}
 
-        // if anything is running
-        if !(len(runningList) == 0) {
-            runningList[0].RemainingStateTime-- // clock tick
-            quant++
-
-            if runningList[0].RemainingStateTime < 1 {  // done running
-                quant = 0
-                v := runningList[0].newProcState()
-                if v == 1 { // if process doesn't need any more time
-                    fmt.Printf("process %s is done\n", runningList[0])
-                    runningList = runningList[1:] // drop it. TODO save finished procs
-                } else { // needs IO
-                    p := runningList[0]
-                    runningList = runningList[1:]
-                    waitingList = append(waitingList, p)
-                }
-            } else if quant == QUANTUM { // quantum is up, preempt the running proc
-                p := runningList[0]
-                p.Preempted = true
-                runningList = runningList[1:]
-                readyList = append(readyList, p)
-                quant = 0
+		removeInd := make([]int, 0)
+        // simulate an IO clock cycle - anything that's in an IO wait is one cycle closer to running
+        for i, _ := range waitingList {
+            waitingList[i].RemainingStateTime--
+            if waitingList[i].RemainingStateTime < 1 {
+                removeInd = append(removeInd, i) // save index of proc that needs IO
             }
         }
-
-        if len(runningList) == 0 {
-            // nothing running - try to get something from the ready queue
-            if !(len(readyList) == 0) {
-                proc := readyList[0]
-                readyList = readyList[1:]
-
-                proc.newProcState()
-                runningList = append(runningList, proc)
-            }
+        // There's got to be a better way of doing this
+        // These are procs that are done w/ IO, ready to run
+        for i := 0; i < len(removeInd); i++ {
+            p := waitingList[i]
+            // cut out the i-th element from waitingList
+            waitingList = append(waitingList[:i], waitingList[i+1:]...)
+            readyList = append(readyList, p)
         }
 
-        time.Sleep(CLOCK_SPEED) // tick tock tick tock
 
-        if len(readyList) == 0 && len(waitingList) == 0 && len(runningList) == 0 {
-            done = true
-        }
-    }
+		// if anything is running
+		if !(len(runningList) == 0) {
+			runningList[0].RemainingStateTime-- // clock tick
+			quant++
+
+			if runningList[0].RemainingStateTime < 1 { // done running
+				quant = 0
+				v := runningList[0].newProcState()
+				if v == 1 { // if process doesn't need any more time
+					fmt.Printf("process %s is done\n", runningList[0])
+					doneList = append(doneList, runningList[0])
+					runningList = runningList[1:] // drop it.
+				} else { // needs IO
+					p := runningList[0]
+					runningList = runningList[1:]
+					waitingList = append(waitingList, p)
+				}
+			} else if quant == QUANTUM { // quantum is up, preempt the running proc
+				p := runningList[0]
+				p.Preempted = true
+				runningList = runningList[1:]
+				readyList = append(readyList, p)
+				quant = 0
+			}
+		}
+
+		if len(runningList) == 0 {
+			// nothing running - try to get something from the ready queue
+			if !(len(readyList) == 0) {
+				proc := readyList[0]
+				readyList = readyList[1:]
+
+				proc.newProcState()
+				runningList = append(runningList, proc)
+			}
+		}
+
+		time.Sleep(CLOCK_SPEED) // tick tock tick tock
+
+		if len(readyList) == 0 && len(waitingList) == 0 && len(runningList) == 0 {
+			done = true
+		}
+	}
+
+	for _, p := range doneList {
+		fmt.Printf("%s waited %d cycles.\n", p, p.WaitTime)
+	}
 }
 
 func printReady(readyList []Proc) {
-    for _, proc := range readyList {
-        fmt.Printf("        %s is waiting\n", proc)
-    }
+	for _, proc := range readyList {
+		fmt.Printf("        %s is waiting\n", proc)
+	}
 }
 
 func printRunning(runningList []Proc) {
-    for _, proc := range runningList {
-        fmt.Printf("%s is running\n", proc)
-    }
+	for _, proc := range runningList {
+		fmt.Printf("%s is running\n", proc)
+	}
 }
 
 func printWaiting(waitingList []Proc) {
-    for _, proc := range waitingList {
-        fmt.Printf("    %s is doing IO\n", proc)
-    }
+	for _, proc := range waitingList {
+		fmt.Printf("    %s is doing IO\n", proc)
+	}
 }
 
 func getInitialProcList() []Proc {
-    p1 := Proc{"P1", []int{7, 2, 9, 6, 10}, 0, false}
-    p2 := Proc{"P2", []int{9, 4, 5, 3, 2}, 0, false}
-    p3 := Proc{"P3", []int{12, 5, 16, 7, 4}, 0, false}
+	p1 := Proc{"P1", []int{7, 2, 9, 6, 10}, 0, false, 0}
+	p2 := Proc{"P2", []int{9, 4, 5, 3, 2}, 0, false, 0}
+	p3 := Proc{"P3", []int{12, 5, 16, 7, 4}, 0, false, 0}
 
-    ret := []Proc{p1, p2, p3}
+	ret := []Proc{p1, p2, p3}
 
-    return ret
+	return ret
 }
--- proc.go
+++ proc.go
@@ -5,27 +5,26 @@ import "fmt"
 type Proc struct {
 	Name               string
 	Times              []int
-    RemainingStateTime int
-    Preempted          bool
+	RemainingStateTime int
+	Preempted          bool
+	WaitTime           int
 }
 
 func (p Proc) String() string {
-    s := fmt.Sprintf("%s", p.Name)
-    return s
+	s := fmt.Sprintf("%s", p.Name)
+	return s
 }
-
 
 func (p *Proc) newProcState() int {
-    if p.Preempted {
-        p.Preempted = false
-        return 0
-    }
-    if len(p.Times) == 0 {
-        return 1
-    }
-    newStateTime := p.Times[0]
-    p.Times = p.Times[1:]
-    p.RemainingStateTime = newStateTime
-    return 0
+	if p.Preempted {
+		p.Preempted = false
+		return 0
+	}
+	if len(p.Times) == 0 {
+		return 1
+	}
+	newStateTime := p.Times[0]
+	p.Times = p.Times[1:]
+	p.RemainingStateTime = newStateTime
+	return 0
 }
-
